5313c8df76494d136f42b457a1451ed28b849aac
sandervandevelde <svelde@gmail.com>
2024-07-08 21:17:20 +0200

    sql script and device client added

---
+++ src/deviceclient/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace AsaTestConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, ASA World!");
+
+            var connectionString = Environment.GetEnvironmentVariable("ASADEVICECLIENT");
+
+            using var deviceClient = DeviceClient.CreateFromConnectionString(connectionString);
+
+            SendMessage(deviceClient);
+        }
+
+        private static void SendMessage(DeviceClient deviceClient)
+        {
+            var messageBody = new MessageBody { deviceId = "sensor-001", temp = 78, pressure = 1001, time = DateTime.Now }; 
+
+            string jsonData = JsonConvert.SerializeObject(messageBody);
+
+            using var message = new Message(Encoding.UTF8.GetBytes(jsonData));
+
+            deviceClient.SendEventAsync(message).Wait();
+
+            Console.WriteLine($"A message is sent: '{jsonData}'");
+        }
+    }
+
+    class MessageBody
+    {
+        public DateTime time { get; set; }
+
+        public double temp { get; set; }
+
+        public double pressure { get; set; }
+
+        public string deviceId { get; set; }
+    }
+}
---
+++ src/sqlserver/script.sql
@@ -0,0 +1,46 @@
+DROP TABLE dbo.DeviceAlerts
+
+
+CREATE TABLE DeviceAlerts (
+    DeviceId nvarchar(255),
+    ClientId nvarchar(255),
+    EmailAddress nvarchar(255),
+    IsEnabled bit,
+	PRIMARY KEY (DeviceId, ClientId)
+)
+
+
+select * from dbo.DeviceAlerts
+
+
+INSERT INTO DeviceAlerts (DeviceId, ClientId, EmailAddress, IsEnabled)
+VALUES ('sensor-001', 'ClientA', 'C.A@company.com', 1);
+
+INSERT INTO DeviceAlerts (DeviceId, ClientId, EmailAddress, IsEnabled)
+VALUES ('sensor-001', 'ClientB', 'C.B@company.com', 0);
+
+INSERT INTO DeviceAlerts (DeviceId, ClientId, EmailAddress, IsEnabled)
+VALUES ('sensor-001', 'ClientC', 'C.C@company.com', 0);
+
+INSERT INTO DeviceAlerts (DeviceId, ClientId, EmailAddress, IsEnabled)
+VALUES ('sensor-001', 'ClientD', 'C.D@company.com', 1);
+
+INSERT INTO DeviceAlerts (DeviceId, ClientId, EmailAddress, IsEnabled)
+VALUES ('sensor-002', 'ClientA', 'C.A@company.com', 1);
+
+INSERT INTO DeviceAlerts (DeviceId, ClientId, EmailAddress, IsEnabled)
+VALUES ('sensor-002', 'ClientB', 'C.B@company.com', 0);
+
+INSERT INTO DeviceAlerts (DeviceId, ClientId, EmailAddress, IsEnabled)
+VALUES ('sensor-002', 'ClientC', 'C.C@company.com', 1);
+
+
+select DeviceId, ClientId, EmailAddress, IsEnabled from dbo.DeviceAlerts where IsEnabled = 1 
+
+
+INSERT INTO DeviceAlerts (DeviceId, ClientId, EmailAddress, IsEnabled)
+VALUES ('sensor-001', 'ClientE', 'C.E@company.com', 1);
+
+delete from dbo.DeviceAlerts where (DeviceId = 'sensor-001') and (EmailAddress = 'C.A@company.com')
+
+
